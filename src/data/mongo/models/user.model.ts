2fe61f219ea5de0a5f69d48a6ed2bc00ac3dbeb1
Daniel H <danielhernandez16@hotmail.com>
2024-02-13 16:11:52 -0500

    feat(mongo): Create user model

---
+++ src/data/mongo/models/user.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+	name: {
+		type: String,
+		required: [true, 'name is required']
+	},
+	email: {
+		type: String,
+		required: [true, 'email is required'],
+		unique: true
+	},
+	password: {
+		type: String,
+		required: [true, 'password is required']
+	},
+	img: {
+		type: String
+	},
+	role: {
+		type: [String],
+		default: ['USER_ROLE'],
+		enum: ['ADMIN_ROLE', 'USER_ROLE']
+	},
+})
+
+export const UserModel = mongoose.model('User', userSchema)
