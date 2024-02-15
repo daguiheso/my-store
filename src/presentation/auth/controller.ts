cb59ed8f9658b94c75f9920070956c3c9a2b1664
Daniel H <danielhernandez16@hotmail.com>
2024-02-14 21:34:40 -0500

    feat(create-user): Add to DB and handle errors

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { RegisterUserDto } from "../../domain";
+import { CustomError, RegisterUserDto } from "../../domain";
 import { AuthService } from "../services/auth.service";
 
 export class AuthController {
@@ -9,6 +9,15 @@ export class AuthController {
 		private readonly authService: AuthService
 	) { }
 
+	private handleError = (error: unknown, res: Response) => {
+		if (error instanceof CustomError) {
+			return res.status(error.statusCode).json({ error: error.message })
+		}
+
+		console.log(error)
+		return res.status(500).json({ error: 'Internal server error' })
+	}
+
 	registerUser = (req: Request, res: Response) => {
 		const [error, registerUserDto] = RegisterUserDto.create(req.body)
 
@@ -16,6 +25,7 @@ export class AuthController {
 
 		this.authService.registerUser(registerUserDto!)
 			.then(user => res.json(user))
+			.catch(error => this.handleError(error, res))
 
 	}
 
--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { UserModel } from "../../data";
-import { CustomError, RegisterUserDto } from "../../domain";
+import { CustomError, RegisterUserDto, UserEntity } from "../../domain";
 
 export class AuthService {
 
@@ -12,6 +12,24 @@ export class AuthService {
 
 		if (userAlreadyExists) throw CustomError.badRequest('Email already exist')
 
-		return 'todo ok'
+		try {
+			const user = new UserModel(dto)
+			await user.save()
+
+			const { password, ...rest } = UserEntity.fromObject(user)
+
+			// Encript password
+
+			// JWT - para maneter la auth del usuario
+
+			// Send confirmation email
+
+			return {
+				user: rest,
+				token: 'ABC'
+			}
+		} catch (error) {
+			return CustomError.internalServer(`${error}`)
+		}
 	}
 }
