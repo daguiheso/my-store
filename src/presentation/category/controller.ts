d38de9e8e1989f19a493ef4a8e92c0440521ddcb
Daniel H <danielhernandez16@hotmail.com>
2024-02-19 20:16:42 -0500

    feat(category): Create routes and controllers

---
+++ src/presentation/category/controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { CustomError } from "../../domain";
+
+export class CategoryController {
+
+	constructor() { }
+
+	private handleError = (error: unknown, res: Response) => {
+		if (error instanceof CustomError) {
+			return res.status(error.statusCode).json({ error: error.message })
+		}
+
+		console.log(error)
+		return res.status(500).json({ error: 'Internal server error' })
+	}
+
+	createCategory = (req: Request, res: Response) => {
+		res.json('CreateCategory')
+	}
+
+	getCategories = (req: Request, res: Response) => {
+		res.json('Categories')
+	}
+}
